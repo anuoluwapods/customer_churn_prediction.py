121dd49ff3f4864df4f9c4bb68b6b42c0cbc08a8
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-02 01:59:00 +0100

    Update 📱Customer-Churn.py

--- app/📱Customer-Churn.py
+++ app/📱Customer-Churn.py
@@ -54,7 +54,7 @@ try:
 # prediction code
     if st.button('Predict'):
         default_check = generateprediction([ Age, Number_of_Dependents, Number_of_Referrals, Tenure_in_Months,
-                                             Monthly_Charge, Total_Charges]])
+                                             Monthly_Charge, Total_Charges])
 
 except:
     pass
