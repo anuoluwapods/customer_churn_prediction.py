cfc248d54968d04b3071a368c8537545d8029a93
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-02 02:24:25 +0100

    Update 📱Customer-Churn.py

--- app/📱Customer-Churn.py
+++ app/📱Customer-Churn.py
@@ -9,9 +9,9 @@ col3, col4 = st.columns(2)
 image = Image.open('image.png')
 
 col1.header("Telecommunication Customer Churn Prediction App ")
-header = '<p style="font-family:sans-serif; color:Grey;">This Web Application can predict if a customer will stay or churn a telecommunication service.</p>'
+header = '<p style="font-family:sans-serif; color:White;">This Web Application can predict if a customer will stay or churn a telecommunication service.</p>'
 col1.markdown(header, unsafe_allow_html=True)
-subheader = '<p style="font-family:sans-serif; color:Grey;">We created this webapp to help reduce the high rate of customer churn in telecommunication industries.</p>'
+subheader = '<p style="font-family:sans-serif; color:White;">We created this webapp to help reduce the high rate of customer churn in telecommunication industries.</p>'
 col1.markdown(subheader, unsafe_allow_html=True)
 col1.write("[![Ayobami's Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/Ayobami6) Ayobami's Page") 
 col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/Designegycreatives/customer_churn_prediction.py) Project Source Code")
